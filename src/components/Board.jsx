38678f1227b004be84bf1cbdc90c37641cf4d85a
lostNIII3 <73011138+NitinSirsath@users.noreply.github.com>
2022-08-04 13:54:50 +0530

    added boardStatus component and added style

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,22 +1,59 @@
 import React , {useState} from 'react'
 import Square from './Square'
 import style from '../styles/board.module.css'
+import BoardStatus from './BoardStatus'
 
 const Board = () => {
 
     const [squares, setSquares] = useState(Array(9).fill(null))
     const [isX, setIsX] = useState(true)
+
+    function calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ]
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i]
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+          }
+        }
+        return null
+      }
+      
     
 
     const handleClick = i => {
         console.log(squares[i]);
-        if(!squares[i]){
-            squares[i] = isX? "X" : 'O'
-            setSquares(squares)
-            setIsX(preV => !preV)
-            console.log(squares[i], i,'after');
-      }
+        if(calculateWinner(squares) || squares[i]){
+            return      
+        }
+        squares[i] = isX? "X" : 'O'
+        setSquares(squares)
+        setIsX(preV => !preV)
     }
+
+        let winner = calculateWinner(squares)
+        let status 
+        if(winner){
+            status = `Winner : ${winner}`
+        }
+        else{
+            status = `Next Player : ${isX? 'X' : 'O'}`
+        }
+
+        const handleRestart = () => {
+            setIsX(true)
+            setSquares(Array(9).fill(null))
+        }
+
 
   return (
     <div>
@@ -35,6 +72,7 @@ const Board = () => {
         <Square value={squares[7]} onClick={() => handleClick(7)}/>
         <Square value={squares[8]} onClick={() => handleClick(8)}/>
         </div>
+        <BoardStatus winner={winner} restart={handleRestart}  status={status}/>
     </div>
   )
 }
---
+++ src/components/BoardStatus.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import style from '../styles/boardStatus.module.css'
+
+const BoardStatus = ({status ,restart,winner}) => {
+  return (
+    <div className={style.container}>
+        <div className={style.status_wrapper}>
+            {status}
+        </div>
+        <div >
+    <button className={style.restart_btn} onClick={restart}>{winner? 'Restart the game' : 'Clear the game'}</button> 
+        </div>
+    </div>
+  )
+}
+
+export default BoardStatus
